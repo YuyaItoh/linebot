75d40a5d51e5f88d2d4984ab100347a185e3e5a1
YuyaItoh <yuya.itoh.from2012@gmail.com>
2016-06-09 22:12:11 +0900

    Apiライブラリをしないように修正

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -8,15 +8,33 @@ class CallbacksController < ApplicationController
     client = Net::HTTP.new(uri.host, 443)
     client.use_ssl = true
 
+    # 
     # 受信
-    # received_body = JSON.parse(request.body.read)["result"][0]
-    received_body = JSON.parse(File.read("test.json"))["result"][0]
+    # 
+    received_body = JSON.parse(request.body.read)["result"][0]
     r_msg = received_body["content"]["text"]
     user = received_body["content"]["from"]
+
+    # 
+    # 送信
+    # 
+    header = {
+      "Content-Type" => "application/json; charser=UTF-8",
+      'X-Line-ChannelID' => CHANNEL_ID,
+      'X-Line-ChannelSecret' => CHANNEL_SECRET,
+      'X-Line-Trusted-User-With-ACL' => MID
+    }
+
     s_msg = r_msg + "だにゃん"
 
-    # 送信
-    Api.send_message(client, user, s_msg)
+    body = {
+      to: [to],
+      toChannel:1383378250,
+      eventType:"138311608800106203",
+      content: { contentType:1, toType:1, text: r_msg } 
+    }
+
+    client.post("/v1/events", body.to_json, header)
 
     render json: "{status: success}"
   end
