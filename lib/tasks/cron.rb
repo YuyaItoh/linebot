05b4276d955082dd2f40f94140ec4a9016ba8a65
YuyaItoh <yuya.itoh.from2012@gmail.com>
2016-06-12 19:02:45 +0900

    cronの処理変更

--- lib/tasks/cron.rb
+++ lib/tasks/cron.rb
@@ -8,6 +8,16 @@ class Cron
 
   # なんか適当にしゃべる
   def self.say_something
+    # テキストの種類
+    texts = [
+      "おねむだにゃん",
+      "おなかすいたにゃん",
+      "極上マグロがほしいにゃん",
+      "ひまだにゃん",
+      "ぽんちゃんだにゃん",
+      "たすのあほー"
+    ]
+
     # URI
     uri = URI.parse('https://trialbot-api.line.me')
     client = Net::HTTP.new(uri.host, 443)
@@ -21,11 +31,13 @@ class Cron
       'X-Line-Trusted-User-With-ACL' => MID
     }
 
+    msg = texts[rand(texts.length)]
+
     body = {
       to: ["u375db29de1dc8806fed21d85a27a057a"],
       toChannel:1383378250,
       eventType:"138311608800106203",
-      content: { contentType:1, toType:1, text: "にゃー" } 
+      content: { contentType:1, toType:1, text: msg } 
     }
 
     client.post("/v1/events", body.to_json, header)
