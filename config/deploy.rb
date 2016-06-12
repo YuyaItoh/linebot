a3c1716b28eed3d3c50bd69ba3326cbe05bd9633
YuyaItoh <yuya.itoh.from2012@gmail.com>
2016-06-12 18:40:58 +0900

    hoge 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.2.0'
 
 # wheneverと連動
-set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # サーバ設定
 server 'yy-sn.com', user: 'y-itoh', role: %w{web app db}
