94a0c6c4bfe92fea10b8433c83e413f1c3c07f90
YuyaItoh <yuya.itoh.from2012@gmail.com>
2016-06-12 18:19:55 +0900

    hoge

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@ set :output, {error: 'log/error_cron.log', standard: 'log/cron.log'}
 set :environment, :development
 
 # スケジュール
-every 20.seconds do
+every 1.minutes do
   # cronのコマンドライン上で動くのでクォートで囲む
   runner 'Cron.say_something'
 end
